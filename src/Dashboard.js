9c28ff06fe775b98e04b5cdec5a4f7312f3c0eba
kazu <test@gmail.com>
2023-10-15 15:52:54 +0900

    登録画面の修正

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -7,10 +7,29 @@ const Dashboard = () => {
     new Date().toISOString().split("T")[0]
   );
   const [selectedTime, setSelectedTime] = useState("12:00");
+  const userId = "12345"; // 仮のユーザーID
+  const attendanceCounts = {
+    // 仮の出欠席データ
+    attendance: 10,
+    late: 2,
+    leaveEarly: 1,
+    absence: 0,
+  };
 
   return (
     <div className="container mt-5">
       <h2 className="text-center mb-5">出席管理ダッシュボード</h2>
+
+      {/* ユーザーID */}
+      <h4 className="mb-4">ユーザーID: {userId}</h4>
+
+      {/* 出欠席回数 */}
+      <div className="mb-4">
+        出席回数: {attendanceCounts.attendance}
+        遅刻回数: {attendanceCounts.late}
+        早退回数: {attendanceCounts.leaveEarly}
+        欠席回数: {attendanceCounts.absence}
+      </div>
 
       {/* ボタン群 */}
       <div className="mb-4 d-flex justify-content-around">
@@ -52,6 +71,31 @@ const Dashboard = () => {
       <div className="text-center">
         <button className="btn btn-primary">保存</button>
       </div>
+
+      {/* 出欠席一覧 */}
+      <h4 className="mt-5 mb-3">受講生の出欠席一覧</h4>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>日付</th>
+            <th>状態</th>
+            <th>時刻</th>
+            <th>操作</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* 以下は仮のデータです */}
+          <tr>
+            <td>2023-10-10</td>
+            <td>遅刻</td>
+            <td>09:10</td>
+            <td>
+              <button className="btn btn-danger">削除</button>
+            </td>
+          </tr>
+          {/* 他のデータも同様にこちらにリストします */}
+        </tbody>
+      </table>
     </div>
   );
 };
