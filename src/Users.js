a6626239fa1143f1b742b60de90fc73221e8ab5d
kazu <test@gmail.com>
2023-10-16 14:32:25 +0900

    spearベースのテスト

--- src/Users.js
+++ src/Users.js
@@ -2,19 +2,26 @@ import React, { useEffect, useState } from "react";
 import supabase from "./supabaseClient";
 
 function Users() {
-  const [users, setUsers] = useState([]);
+  const [records, setRecords] = useState([]);
 
   useEffect(() => {
     async function fetchUsers() {
-      // 全てのカラムを選択
-      const { data: Students, error } = await supabase
-        .from("students")
-        .select("*");
+      const { data, error } = await supabase.from("students").select(
+        `
+          student_id,
+          login_id,
+          name,
+          created_at,
+          updated_at,
+          generation_num,
+          attendance_records:attendance_records (date, status, time)
+          `
+      );
 
       if (error) {
-        console.error("Error fetching users:", error);
+        console.error("Error fetching records:", error);
       } else {
-        setUsers(Students);
+        setRecords(data);
       }
     }
     fetchUsers();
@@ -23,15 +30,22 @@ function Users() {
   return (
     <div>
       <h1>Users</h1>
-      {users.map((user) => (
-        <div key={user.student_id}>
-          <p>Student ID: {user.student_id}</p>
-          <p>Login ID: {user.login_id}</p>
-          <p>Name: {user.name}</p>
-          <p>Created At: {user.created_at}</p>
-          <p>Updated At: {user.updated_at}</p>
-          <p>Generation Num: {user.generation_num}</p>
-          {/* 必要に応じて他のカラムも追加できます */}
+      {records.map((record) => (
+        <div key={record.student_id}>
+          <p>Student ID: {record.student_id}</p>
+          <p>Login ID: {record.login_id}</p>
+          <p>Name: {record.name}</p>
+          <p>Created At: {record.created_at}</p>
+          <p>Updated At: {record.updated_at}</p>
+          <p>Generation Num: {record.generation_num}</p>
+          <h2>Attendance Records</h2>
+          {record.attendance_records.map((attendance, index) => (
+            <div key={index}>
+              <p>Date: {attendance.date}</p>
+              <p>Status: {attendance.status}</p>
+              <p>Time: {attendance.time || "N/A"}</p>
+            </div>
+          ))}
         </div>
       ))}
     </div>
