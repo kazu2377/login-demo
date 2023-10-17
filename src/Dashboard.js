e1cf5865be823e7751a80e7bf6217209dbdeb10a
kazu <test@gmail.com>
2023-10-17 13:51:50 +0900

    削除論理を追加合わせてフラグ0だけ抽出

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,7 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import supabase from "./supabaseClient";
+//... (他のimport文は変わらず)
+
+import { toast } from "react-toastify"; // 通知メッセージの表示のため
+import "react-toastify/dist/ReactToastify.css"; // 通知スタイル
 
 const Dashboard = ({ studentId }) => {
   const [selectedDate, setSelectedDate] = useState(
@@ -18,32 +22,71 @@ const Dashboard = ({ studentId }) => {
     absence: 0,
   };
 
-  useEffect(() => {
-    async function fetchAttendanceRecordsForStudent() {
-      const { data, error } = await supabase
-        .from("students")
-        .select(
-          `
-          student_id,
-          attendance_records:attendance_records (
-              record_id,
-              date,
-              status,
-              time
-          )
-      `
-        )
-        .eq("student_id", studentId);
+  const fetchAttendanceRecordsForStudent = useCallback(async () => {
+    const { data, error } = await supabase
+      .from("students")
+      .select(
+        `
+    student_id,
+    attendance_records:attendance_records(record_id, date, status, time)
+  `
+      )
+      .eq("student_id", studentId)
+      .eq("generation_num", 0)
+      .filter("attendance_records.generation_num", "eq", 0);
 
-      if (error) {
-        console.error("Error fetching attendance records:", error);
-      } else if (data && data[0] && data[0].attendance_records) {
-        setAttendanceRecords(data[0].attendance_records);
-      }
+    // const { data, error } = await supabase
+    //   .from("students")
+    //   .select(
+    //     `
+    //     student_id,
+    //     attendance_records:attendance_records (
+    //         record_id,
+    //         date,
+    //         status,
+    //         time
+    //     )
+    // `
+    //   )
+    //   .eq("student_id", studentId)
+    //   .eq("generation_num", 0)
+    //   .filter("attendance_records", "generation_num", "eq", 0); // この行を追加
+
+    if (error) {
+      console.error("Error fetching attendance records:", error);
+    } else if (data && data[0] && data[0].attendance_records) {
+      setAttendanceRecords(data[0].attendance_records);
     }
-
-    fetchAttendanceRecordsForStudent();
   }, [studentId]);
+
+  useEffect(() => {
+    fetchAttendanceRecordsForStudent();
+  }, [fetchAttendanceRecordsForStudent]);
+
+  async function deleteRecord(recordId) {
+    try {
+      // const { error } = await supabase
+      //   .from("attendance_records")
+      //   .delete()
+      //   .eq("record_id", recordId);
+
+      // if (error) {
+      //   throw error;
+      // }
+
+      await supabase
+        .from("attendance_records")
+        .update({ generation_num: 1 })
+        .eq("record_id", recordId);
+
+      toast.success("データを削除しました。");
+
+      fetchAttendanceRecordsForStudent();
+    } catch (error) {
+      console.error("Error deleting record:", error);
+      toast.error("データの削除に失敗しました。");
+    }
+  }
 
   return (
     <div className="container mt-5">
@@ -105,7 +148,12 @@ const Dashboard = ({ studentId }) => {
               <td>{record.status}</td>
               <td>{record.time || "なし"}</td>
               <td>
-                <button className="btn btn-danger">削除</button>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => deleteRecord(record.record_id)}
+                >
+                  削除
+                </button>
               </td>
             </tr>
           ))}
