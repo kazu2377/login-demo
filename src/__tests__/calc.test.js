932e908795629e2bb05317e9392abdc920266f63
kazu <test@gmail.com>
2023-10-19 17:59:49 +0900

    App.test.js

--- src/__tests__/calc.test.js
+++
@@ -1,5 +0,0 @@
-const addNumbers = require("./calc");
-
-test("One plus two should be 3", () => {
-  expect(addNumbers(1, 2)).toBe(3);
-});
--- src/__tests__/candyShop.test.js
+++
@@ -1,9 +0,0 @@
-// candyShop.test.js ファイル
-const sellCandy = require("./candyShop");
-const mockSellCandy = jest.fn(sellCandy);
-
-test("should sell candy", () => {
-  mockSellCandy("Chocolate");
-  expect(mockSellCandy).toHaveBeenCalledWith("Chocolate");
-  expect(mockSellCandy).toHaveBeenCalledTimes(1);
-});
